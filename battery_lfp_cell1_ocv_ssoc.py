10baa857a86ac990fd97c23923f15bdfc1e0301a
mahadev-hash <mahadev@thinkclock.com>
2026-03-11 13:30:38 +0530

    Update battery_lfp_cell1_ocv_ssoc.py

--- battery_lfp_cell1_ocv_ssoc.py
+++ battery_lfp_cell1_ocv_ssoc.py
@@ -219,6 +219,8 @@ def update_graph(cell_a, cell_b, mode):
 
 # ================= RUN =================
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8050))
+    app.run(host="0.0.0.0", port=port)
+
 
 
